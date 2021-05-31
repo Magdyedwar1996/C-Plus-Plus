5f72bfe1c7a8b0f1538423df22d84f7fcefc556b
magdy_edwar <magdyedwar1996@gmail.com>
2021-06-01 00:42:02 +0200

    a bit change

--- 02_GeeksForGeeks/01_Find the output/01_Set one/problem_2.cpp
+++ 02_GeeksForGeeks/01_Find the output/01_Set one/problem_2.cpp
@@ -5,19 +5,25 @@ using namespace std;
 class Base1 {
 public:
 	Base1()
-	{ cout << " Base1's constructor called" << endl; }
+	{
+		 cout << " Base1's constructor called" << endl;
+    }
 };
 
 class Base2 {
 public:
 	Base2()
-	{ cout << "Base2's constructor called" << endl; }
+	{
+		 cout << "Base2's constructor called" << endl;
+    }
 };
 
 class Derived: public Base1, public Base2 {
 public:
 	Derived()
-	{ cout << "Derived's constructor called" << endl; }
+	{ 
+		cout << "Derived's constructor called" << endl; 
+	}
 };
 
 int main()
